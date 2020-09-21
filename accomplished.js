f74c8cb44dae5526af1637ac6bc6ff2a5c3c45a9
liltyrese <tyreseet@gmail.com>
2020-09-21 10:47:19 -0400

    Initial commit

---
+++ accomplished.js
@@ -0,0 +1,8 @@
+const list = document.getElementById("list")
+
+function addTodo(todo){
+    const text = `<li class = "item">
+        <i></i>
+        <p class = "text ${todo}"></p>
+    </li>`
+}
